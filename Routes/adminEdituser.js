f6bc1826b3ff6684520d588a748d8dc74d515963
Alexandra <alexandervirtuous14@gmail.com>
2024-06-22 12:00:35 +0100

    modified:   Routes/adminEdituser.js

--- Routes/adminEdituser.js
+++ Routes/adminEdituser.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-require("../Schemas/UsersDetails");
+require("../Schemas/UserDetails");
 
 const User = mongoose.model("UserInfo");
 
