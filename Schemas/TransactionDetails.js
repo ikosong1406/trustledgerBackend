282f06e254bc65fabad643df8dd4beaf68cf9155
Alexandra <alexandervirtuous14@gmail.com>
2024-06-25 13:17:27 +0100

    modified:   Schemas/TransactionDetails.js

--- Schemas/TransactionDetails.js
+++ Schemas/TransactionDetails.js
@@ -2,6 +2,11 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   amount: {
     type: Number,
     required: true,
