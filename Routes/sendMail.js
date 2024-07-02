78ae114b1efd3fe761d37c90b524f4ee63e39744
Alexandra <alexandervirtuous14@gmail.com>
2024-07-02 22:08:04 +0100

    modified:   Routes/sendMail.js

--- Routes/sendMail.js
+++ Routes/sendMail.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const sendEmail = require("../utils/mail");
+const { sendMail } = require("../utils/mail");
 
 router.post("/", async (req, res) => {
   try {
-    const { to, subject, body } = req.body;
-    await sendEmail(to, subject, body);
+    const { to, subject, text } = req.body;
+    await sendMail(to, subject, text, "");
     res.status(200).send({ message: "Email sent successfully!" });
   } catch (error) {
     console.error("Error sending email:", error);
